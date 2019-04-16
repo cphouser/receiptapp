129151d9cbb55cc7cea30a7c76425c00c0a0fa43
calvin houser <calvin@WindowsNT.calnet>
2019-04-16 11:18:44 -0700

    added more to price checking

--- receipt2csv.py
+++ receipt2csv.py
@@ -32,24 +32,27 @@ def acertainDateValue(date_string):
         return None
     else: return datetime(year+CENTURY,month,day,hour,minute)
 
-def isolateDate(date_candidates):
-    """
-    Parses a list of potential date strings into a list of datetime objects.
-    If more than one valid date is found, prompts user to select.
-    """
-    dates = []
-    for candidate in date_candidates:
-        date = acertainDateValue(candidate)
-        if date is not None:
-            dates.append(date)
+def acertainPriceValue(price_string):
+    def lastDigit(string):
+        for idx in range(-1, -len(string), -1):
+            if string[idx].isdigit(): return len(string) + idx; 
+            elif len(string) + idx == 0: return -1
+    def firstDigit(string):
+        for idx in range(-1, -len(string), -1):
+            if string[idx].isdigit(): continue
+            else: return 
 
-    if len(dates) > 1 and not all(date == dates[0] for date in dates):
-        while True:
-            print("Multiple date values found.")
-            for index,date in enumerate(dates): print('['+str(index)+'] ',date)
-            num = int(input("Line number of the correct date:"))
-            if num >= 0 and num < len(dates): return dates[num]
-    else: return dates[0] if len(dates) > 0 else None
+    price_string = price_string.replace(',','.')
+    i = price_string.rfind('.')
+    j = price_string[:i].rfind(' ') if i != -1 \
+            else price_string.rfind(' ')
+    k = lastDigit(price_string)
+    if j == -1: j = 0
+    if k == -1: return None
+    if i == -1:
+        if price_string[j:k+1].isdigit(): return int(price_string[j:k+1])
+        else: i = firstDigit(price_string[:k]); return int(price_string[i:k+1]) 
+    return int(price_string[j+1:i])*100 + int(price_string[i+1:i+3])
 
 def tesseractImage(filepath):
     """
@@ -70,7 +73,6 @@ def parseLine(line):
         Check each line for a price (at least 4 numeric characters at end)
         """
         letter_flag = 0
-        #iterate backwards through the string for first alphabet character
         for i in range(-1, -len(line), -1):
             if line[i].isalpha():
                 #detect first alphabet character
@@ -79,15 +81,12 @@ def parseLine(line):
                 #if second alphabet char is less than 4 
                 #chars left of the first there isn't a price
                 if letter_flag == -1 and -(i - letter_flag) < 4:
-                    #consider line as heading (no price)
-                    return (line,None)
-
+                    if all((i.isupper() if i.isalpha() else True) for i in line):
+                        return (line,None) 
+                    else: break
                 else:
-                    #consider line as item (name,price pair)
                     split = i+2 if i+1 == letter_flag else i+1
                     return (line[:split], line[split:])
-                    #    name = line[:i+2]; price = line[i+2:]
-                    #else: name = line[:i+1]; price = line[i+1:]
         return (None,line)
 
     if len(line) < 4:
@@ -103,6 +102,39 @@ def parseLine(line):
         return (None,None,None,None)
     else: return (name,price,None,None)
     
+def priceCheck(items):
+    _, balance, _ = items.pop()
+    price_list = [item[1] for item in items]
+    price_sum = sum(price_list)
+    print("balance is: ", balance, " sum(price_list) is: ", price_sum)
+    if price_sum == balance: 
+        print("prices correct!") 
+        return
+    elif price_sum > balance:
+        for price in price_list:
+            all_others = price_sum - price
+            if all_others == balance:
+                print(price, "should be removed from sum")
+            possible_price = balance - all_others
+            print("without ",price , ", sum(price_list) is: ", all_others,
+                    " (should it be ", possible_price, "?)")
+            if str(possible_price) in str(price):
+                print(price, " should be ", possible_price)
+                break;
+        #if good corrections exits, then
+        return
+    #else:
+
+    for price in price_list:
+        all_others = price_sum - price
+        #print("all others ", all_others)
+        possible_price = balance - all_others
+        print("comparing ", price, " to ", possible_price)
+        #if fuzz.ratio(str(possible_price),str(price)) > 80:
+        #    print("correcting ", price, " to ", possible_price)
+        #    price = possible_price
+        #    break
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -117,7 +149,7 @@ if __name__ == '__main__':
         img_path = sys.argv[i]
         lines = tesseractImage(img_path).splitlines()
 
-        category_head = ''
+        category_head = None
         items = []
         dates = []
         for line in lines:
@@ -125,23 +157,35 @@ if __name__ == '__main__':
             if date is not None: dates.append(date); continue
             if category is not None: category_head = category; continue
             if name is not None and len(name) > 1:
-                items.append((name,price,category_head)) \
-                        if category_head.isupper() \
-                        else items.append((name,price,None)); continue
+                items.append((name,price,category_head)); continue
             #print('DISCARDING LINE \'',line,'\' (could not be parsed)')
 
         items_prime = []
-        for name,*tup in items:
-            not_items = ('Regular Price','Card Savings')
-            list_end = ('BALANCE','TOTAL')
+        coupon_list = []
+        for name,price,category in items:
+            not_items = ['Regular Price','Card Savings']
+            coupon_names = ['Store Coupon']
+            list_end = ['BALANCE','TOTAL']
             if any(fuzz.ratio(string,name) > 80 for string in not_items):
                 continue
-            print(name,*tup)
-            items_prime.append((name,*tup))
+            #print('comparing ', name,' to ',coupon_names[0],' fuzz ratio: ',
+            #        fuzz.ratio(coupon_names[0],name))
+            if any(fuzz.ratio(string,name) > 80 for string in coupon_names):
+                coupon_list.append((name,price,category))
+                continue
+            if category is None: continue
+            try:
+                int_price = acertainPriceValue(price)
+                print(name,'\t',int_price,'\t',category)
+                items_prime.append((name,int_price,category))
+            except ValueError:
+                print('Could not parse ',price,' as int')
             if any(fuzz.partial_ratio(strng,name) > 90 for strng in list_end):
                 print('ENDING LIST AT \'',name,'\''); break
 
-        while dates == []: #dates = [None]
+        priceCheck(items_prime)
+
+        if dates == []: #dates = [None]
             print('No date value found. If you have a date enter it here')
             date = input('in the form mm/dd/yy hh:mm (else leave blank) =>')
             if len(date) > 1:
@@ -160,6 +204,7 @@ if __name__ == '__main__':
                 if num >= 0 and num < len(dates): 
                     dates = [date]; break 
         
+
 
         #snippet adapted from "Github Gist - write tuples to csv"
         #https://gist.github.com/agoops/dd3ec3821438b695f7c462877a0fbeb4
